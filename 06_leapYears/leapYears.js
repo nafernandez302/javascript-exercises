a5d24f7dd093b363bc2284ee365a9b122895d6be
Nahuel Fernandez <nafernandez@mi.unc.edu.ar>
2023-12-28 16:10:01 -0300

    terminado 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	let isLeap;
+	if (year%400==0 || (year%100!=0 && year%4==0)) {
+		isLeap = true;
+	}
+	else{
+		isLeap = false;
+	}
+	return isLeap;
 };
 
 // Do not edit below this line
