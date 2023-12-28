0c575766947fe1b3c2677f8b74af3785282791c4
Nahuel Fernandez <nafernandez@mi.unc.edu.ar>
2023-12-28 15:26:32 -0300

    02, 03 terminados

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+	let toPrint
+	if (n>=0) {
+		toPrint = ''
+		for (var i = 0; i < n; i++) {
+			toPrint = toPrint.concat(word)
+		}	
+	}
+	else{
+		toPrint = "ERROR"
+	}
+	return toPrint
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+	let aux = ''
+	let n = word.length-1;
+	for (var i = 0; i <=n; i++) {
+		aux = aux + word[n-i];
+	}
+	return aux;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
