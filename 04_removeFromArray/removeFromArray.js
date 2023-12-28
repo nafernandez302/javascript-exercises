0c18c8f6cb4cdc017a70315c0ab8a7cd0ce53bc7
Nahuel Fernandez <nafernandez@mi.unc.edu.ar>
2023-12-28 15:54:54 -0300

    npm installed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elems) {
+	let newArray = [];
 
+
+	for (var j = 0 ; j < array.length; j++) {
+		let b = true;
+		for(var i = 0; i < elems.length; i++){
+			b = (array[j] != elem[i]) && b
+		}
+		if (b) {
+				newArray.push(array[j]);
+		}
+	}
+	return newArray;
 };
 
 // Do not edit below this line
