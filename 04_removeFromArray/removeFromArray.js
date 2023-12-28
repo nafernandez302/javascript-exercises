f93421a89b6c5ad481962536db566485961a04db
Nahuel Fernandez <nafernandez@mi.unc.edu.ar>
2023-12-28 15:57:30 -0300

    terminado 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,7 +5,7 @@ const removeFromArray = function(array, ...elems) {
 	for (var j = 0 ; j < array.length; j++) {
 		let b = true;
 		for(var i = 0; i < elems.length; i++){
-			b = (array[j] != elem[i]) && b
+			b = (array[j] !== elems[i]) && b
 		}
 		if (b) {
 				newArray.push(array[j]);
