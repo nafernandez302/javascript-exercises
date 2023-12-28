d4d91ea33543b9b06bf3a7ae8d1734534e99bdcd
Nahuel Fernandez <nafernandez@mi.unc.edu.ar>
2023-12-28 16:22:11 -0300

    terminado 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	if(n<0){
+		return "OOPS";
+	}
+	let arr = [];
+	arr[0]=0;
+	arr[1]=1;
+	for (var i = 2; i <=n; i++){
+		arr[i] = arr[i-1] + arr[i-2];
+	}
+	return arr[n];
 };
 
 // Do not edit below this line
